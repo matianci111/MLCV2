8336db885aa362d08b87eed12cfe998089e1109d
Lizhang Lin <lizhanglin@dyn1168-177.wlan.ic.ac.uk>
2017-04-07 14:35:32 +0100

    changed getDescriptor and getInterestPoint

--- getDescriptor.m
+++ getDescriptor.m
@@ -3,10 +3,12 @@ function [patches] = getDescriptor(coords, img)
 %2b) Obtain Descriptor
 %this function takes pairs of coordinates and returns the 31x31 pixel
 %patches around the given coordinates
-G = rgb2gray(img);
+if size(size(img)) == 3
+    img = rgb2gray(img);
+end
 edges = 1:2:256;
 for i=1:size(coords, 1)
-    patch = G(coords(i,2)-15:coords(i,2)+15,coords(i,1)-15:coords(i,1)+15);
+    patch = img(coords(i,1)-15:coords(i,1)+15,coords(i,2)-15:coords(i,2)+15);
     N = histcounts(patch, edges, 'Normalization', 'probability');
     patches(i,:) = N(1,:);
 end
